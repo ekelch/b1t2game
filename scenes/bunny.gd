34a8f3a54d2619097bcdf03fba43e4e15c4590c3
Evan <evan.c.kelch@gmail.com>
2025-08-12 21:21:54 -0500

    changed color model to HSV and added export ranges

--- scenes/bunny.gd
+++ scenes/bunny.gd
@@ -1,34 +1,40 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -320.0
+const SPEED = 260.0
+const JUMP_VELOCITY = -400.0
+const BOOST_MODIFIER = 1.4
+const GRAV_MOD_SCALAR = 2.5
+
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var floor_timer: Timer = $FloorTimer
-var can_jump: bool = false
 
+var can_jump: bool = false
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor() && !can_jump:
 		can_jump = true
 		
 	if !is_on_floor():
-		velocity += get_gravity() * delta
+		var grav = get_gravity() * delta
+		var gravmod = clampf(abs(velocity.y * delta * GRAV_MOD_SCALAR), 0, 12.)
+		grav.y += gravmod
+		velocity += grav
 		if floor_timer.is_stopped():
 			floor_timer.start()
 	
-	if Input.is_action_just_pressed("jump") && can_jump:
+	if Input.is_action_just_pressed("jump"):
 		jump()
-		can_jump = false
 
 	var direction := Input.get_axis("left", "right")
-	handle_direction(direction)
+	var boosted := Input.is_action_pressed("boost")
+	handle_direction(direction, boosted)
 
 	move_and_slide()
 
-func handle_direction(direction: float):
+func handle_direction(direction: float, boosted: bool):
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * (BOOST_MODIFIER if boosted else 1.)
 		sprite.play("running")
 		sprite.flip_h = true if direction < 0 else false
 	else:
@@ -36,7 +42,9 @@ func handle_direction(direction: float):
 		sprite.play("idle")
 
 func jump():
-	velocity.y = JUMP_VELOCITY
+	if can_jump:
+		velocity.y = JUMP_VELOCITY
+		can_jump = false
 
 func _on_floor_timer_timeout() -> void:
 	can_jump = false
--- scenes/level.gd
+++ scenes/level.gd
@@ -1,14 +1,26 @@
 extends Node2D
 
+const hue_start: float = 23
+const hue_end: float = 125
+
+@export_group("hsv")
+@export_range(0.0, 1.0, 0.01)  var hsv_s: float
+@export_range(0.0, 1.0, 0.01) var hsv_v: float
+@export_range(0.0, 1.0, 0.01)  var hsv_a: float = 1.0
+
 @onready var bunny: CharacterBody2D = $Bunny
-@onready var start_pos: Marker2D = $StartPos
+@onready var tileset: TileMapLayer = $TileMapLayer
+@onready var start_pos: Marker2D = $Markers/StartPos
+@onready var end_pos: Marker2D = $Markers/EndPos
+
 var delta_y: float = 0
-const max_y = 1000
 
 func _process(_delta: float) -> void:
-	delta_y = (start_pos.global_position.y - bunny.global_position.y) / max_y
-	var mod = 1 - clamp(delta_y, start_pos.global_position.y, max_y)
-	bunny.modulate = Color(mod, 1., mod, 1.)
+	delta_y = (start_pos.global_position.y - bunny.global_position.y) / -end_pos.global_position.y
+	var mod = clamp(delta_y, 0, 1)
+	var hsv_h = (hue_start + mod * (hue_end - hue_start))/255
+	bunny.modulate = Color.from_hsv(hsv_h, hsv_s, hsv_v, hsv_a)
+	tileset.modulate = bunny.modulate
 
 func _on_reset_timer_timeout() -> void:
 	get_tree().reload_current_scene()
